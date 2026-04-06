7c465aa21bca8ea110ae12ad77c28ab37ea64a90
Aryan Patel <aryan007.lko@gmail.com>
2026-04-07 05:28:57 +0530

    feat: initialize express application with middleware, CORS configuration, and API routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,10 @@ import authRouter from "./routes/auth.routes.js";
 import chatRouter from "./routes/chat.routes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 // middleware
