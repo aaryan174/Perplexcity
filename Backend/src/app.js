c2861018e69eeb1b3a6a66b8f89b78a19703e62a
Aryan Patel <aryan007.lko@gmail.com>
2026-04-07 05:47:22 +0530

    feat: initialize express server and configure static file serving for frontend deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -25,8 +25,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/chats", chatRouter );
 
 app.use(express.static(path.join(__dirname, "public")));
-app.get(/.*/,(req, res)=>{
-    res.sendFile(path.join(__dirname, "public", "index.html"))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 
