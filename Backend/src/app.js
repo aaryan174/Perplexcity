fb8c5cdbdb684c592fe0c2610059584d1a528589
Aryan Patel <aryan007.lko@gmail.com>
2026-04-07 05:50:31 +0530

    feat: initialize Express application with middleware, CORS configuration, and API routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -25,7 +25,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/chats", chatRouter );
 
 app.use(express.static(path.join(__dirname, "public")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
